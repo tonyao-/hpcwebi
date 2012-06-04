4e2dc293acfd1b1e908941c6811a6cdf5ce46023
davidarmesc <davidarmesc@gmail.com>
2012-06-04 10:53:51 +0000

    Inicio

---
+++ permisos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chown -R david:www-data GestorColasPHP
+chmod g+w -R GestorColasPHP/inc/ajaxplorer/data/tmp GestorColasPHP/inc/ajaxplorer/data/cache GestorColasPHP/inc/ajaxplorer/data/logs GestorColasPHP/inc/ajaxplorer/data/public
+
